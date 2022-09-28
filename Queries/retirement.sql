0455c26fe595396e2535eee37f071bb69b72cdfb
Ninotshka Cruz <ninotshkacruz@Ninotshkas-MacBook-Pro.local>
2022-09-28 18:55:31 -0500

    add doc

--- Queries/retirement.sql
+++ Queries/retirement.sql
@@ -37,3 +37,171 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    employee_departments.to_date
+FROM retirement_info
+LEFT JOIN employee_departments
+ON retirement_info.emp_no = employee_departments.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN employee_departments as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN employee_departments as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO current_emp_count
+FROM current_emp as ce
+LEFT JOIN employee_departments as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT emp_no,
+    first_name,
+    last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
+
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN employee_departments as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+SELECT * From retirement_info
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN employee_departments AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--List of retirement_info table, only tailored for the Sales team
+SELECT re.emp_no,
+re.first_name,
+re.last_name,
+de.dept_no
+FROM retirement_info as re
+INNER JOIN employee_departments as de
+ON (re.emp_no=de.emp_no) 
+WHERE de.dept_no = ('d007');
+
+SELECT * FROM departments
+
+SELECT re.emp_no,
+ re.first_name,
+ re.last_name,
+ de.dept_no,
+ d.dept_name
+FROM retirement_info as re
+INNER JOIN employee_departments as de
+ON (re.emp_no=de.emp_no) 
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
+WHERE de.dept_no IN ('d007','d005');   
+
+
+--Create total employees table with title by emp_no
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
+       e.first_name,
+       e.last_name,
+	   t.title,   
+	   t.to_date
+INTO employee_title
+FROM employees as e
+INNER JOIN title as t
+ON e.emp_no = t.emp_no
+WHERE t.to_date = ('9999-01-01')
+ORDER BY e.emp_no,t.to_date DESC
+
+--Retrieve the number of employees by their most recent job title 
+SELECT COUNT(title),et.title
+INTO employee_title_count
+FROM employee_title as et
+GROUP BY title
+ORDER BY COUNT DESC
+
+SELECT COUNT(title),me.title
+--INTO mentorship_elegibility_count
+FROM mentorship_elegibility as me
+GROUP BY title
+ORDER BY COUNT DESC
--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# Pewlett-Hackard-Analysis
+Analysis
+
+## Overview
+ 
+Pewlett Hackard in a large company who have thousands of employees. The Human Resources Department is getting ready for a possible mass employee retirement in the next few years. Upcoming retirement would leave a thousand of job opening therefore Human Resources must conduct an employee research to determinate who is going to retire in the next few years and how many position need to be filled. To perform this task they need to generate a list of the employee eligible for retirement packege by building an employee database in SQL , applying data modeling, engineering and analysis skills.
+
+## Purpose
+ 
+Create and analyze employees database , discover number of retiring employees per title, and identify employees who are eligible to participate in a mentorship program which is a program with experienced and successful employees who will step back into a part-time role mentoring newly hired folks instead of retiring completely.
+
+
+## Results: 
+
+### Deliverable 1: The Number of Retiring Employees by Title 
+
+#### Create a Retirement Title table by to filter retiring employee from the rest of the employees;
+
+
+
+
+- Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+
+- Retrieve the title, from_date, and to_date columns from the Titles table.
+
+- Use INTO clause to create the table
+
+- Join both tables on the primary key (emp_no)
+
+- Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955. Then, order by the employee number.
+
+
+#### Create a Unique Title label that will hold Number of Retiring Employees by Title without duplicate information
+
+
+
+
+- Retrieve the employee number, first and last name, and title columns from the Retirement Titles table.
+
+- Use the DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+
+- Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are equal to '9999-01-01'.
+
+- Use INTO clause to create the table
+
+- Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date) of the most recent title.
+
+
+#### Create rerouting title table with the number of employees by their most recent job title who are about to retire.
+
+
+
+
+
+- Retrieve the number of titles from the Unique Titles table.
+
+- Use INTO clause to create the table to hold the required information.
+
+- Group the table by title, then sort the count column in descending order.
+
+
+### Deliverable 2: The Employees Eligible for the Mentorship Program 
+
+
+
+
+- Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+
+- Retrieve the from_date and to_date columns from the Department Employee table.
+
+- Retrieve the title column from the Titles table.
+
+- Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+
+- Join the Employees and the Department Employee tables on the primary key.
+
+- Filter the data on the to_date column to all the current employees, then filter the data on the birth_date columns to get all the employees whose birth dates are between January 1, 1965 and December 31, 1965
+
+- Join the Employees and the Titles tables on the primary key.
+
+- Order the table by the employee number.
+
+
+
+## Summary: 
+
+
+### How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+
+Human Resources will need to recruit and fill out 72,458 positions in the next few years.
+
+### Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees? 
+
+There is enough mentors for Senior Staff, Senior Engineer, Staff, Technique Leader and Assistance Engineer. However, there is none mentor for Managers (refer to mentorship title count table)
+
